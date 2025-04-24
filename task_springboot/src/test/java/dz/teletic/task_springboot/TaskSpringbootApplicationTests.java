547e75d4277cf4224c922d6533fb9a0e483a7a28
kader_aek <klalibabdelkader@gmail.com>
2025-04-24 20:51:47 +0100

    Setup springboot App

---
+++ task_springboot/src/test/java/dz/teletic/task_springboot/TaskSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.teletic.task_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
